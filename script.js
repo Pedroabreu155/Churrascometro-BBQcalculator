26165c807300ebc5227770a278c55d9255cde2ed
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-03-13 00:03:23 -0300

    htmls elements called in variables

---
+++ script.js
@@ -0,0 +1,15 @@
+// calling the html elements
+
+let inputAdults = document.getElementById("adults")
+let inputChild = document.getElementById("child")
+let inputTime = document.getElementById("time")
+
+// console.log(inputAdults, inputChild, inputTime)
+
+//calling the result div and setting the function
+let result = document.getElementById("result")
+
+function calc(){
+    console.log("Calculando ...")
+}
+
